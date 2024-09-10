96ed97d2fd86c1356a93f6242992a088a7d7b609
Inhwan Kim <a9327370@gmail.com>
2024-09-10 23:53:32 +0900

    Initial Commit

---
+++ MeetTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetTCA
+//
+//  Created by 김인환 on 9/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetTCA/MeetTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetTCAApp.swift
+//  MeetTCA
+//
+//  Created by 김인환 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MeetTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
